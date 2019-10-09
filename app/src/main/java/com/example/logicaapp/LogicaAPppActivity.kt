d120e6dcf7aade768ff392d6eb3b022114691bd6
Dean van Doorn <dean@code-orange.nl>
2019-10-09 02:01:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/logicaapp/LogicaAPppActivity.kt
@@ -0,0 +1,56 @@
+package com.example.logicaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_logica_app.*
+
+class LogicaAPppActivity : AppCompatActivity() {
+
+    private var currentThrow: String = "ff"
+    private var lastThrow: String = "T"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logica_app)
+        initViews()
+    }
+
+    /**
+     * Set the initial (UI) state of the game.
+     */
+    private fun initViews() {
+        btn_confirm.setOnClickListener {
+            if (isCorrect()) {
+                onAnswerCorrect()
+            } else {
+                onAnswerIncorrect()
+            }
+        }
+    }
+
+    private fun isCorrect(): Boolean {
+        val answer_tt = tt.text.toString()
+        val answer_tf = tf.text.toString()
+        val answer_ft = ft.text.toString()
+        val answer_ff = ff.text.toString()
+        return (answer_tt == "T" &&
+                answer_tf == "F" &&
+                answer_ft == "F" &&
+                answer_ff == "F")
+    }
+
+    /**
+     * Displays a successful Toast message.
+     */
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Displays a incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT).show()
+    }
+}
